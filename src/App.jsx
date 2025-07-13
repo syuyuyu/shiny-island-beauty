d1dee9f48c74a028cac5bead29b665ddbb9ebe0b
Syuan Liu <syuan.haha@gmail.com>
2025-07-13 15:11:04 +0800

    更新轉導方式

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import About from './pages/About';
 import Item from './pages/Item';
@@ -12,12 +12,15 @@ const App = () => {
     <BrowserRouter basename="/shiny-island-beauty">
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="about" replace />} />
           <Route path="about" element={<About />} />
           <Route path="item" element={<Item />} />
           <Route path="product" element={<Product />} />
           <Route path="reserve" element={<Reserve />} />
           <Route path="location" element={<Location />} />
         </Route>
+          {/* fallback 處理 */}
+        <Route path="*" element={<Navigate to="about" replace />} />
       </Routes>
     </BrowserRouter>
   );
